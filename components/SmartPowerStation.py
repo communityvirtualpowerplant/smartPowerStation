be11951c83e318da7cc359a9fa3e9427d2421c49
Alex Nathanson <alex@alexnathanson.com>
2025-04-15 19:19:16 -0400

    formatting

--- components/SmartPowerStation.py
+++ components/SmartPowerStation.py
@@ -5,47 +5,47 @@ from typing import cast
 from typing import Any, Dict, Optional, Tuple, List
 
 class SmartPowerStation():
-	def __init__(self, conf: str):
-		self.config = self.getConfig(conf)
-		self.name = self.config['location']
-		self.location = self.config['location']
-		self.promise = self.config['promise']
-		self.network = self.config['network']
-		self.printInfo = True
-		self.printDebug = True
-		self.printError = True
-		self.dataFilePrefix = 'sps'
-		self.shellySTR = 'Shelly'
-		self.bluettiSTR = ['AC180','AC2']
-		self.devices = []
+    def __init__(self, conf: str):
+        self.config = self.getConfig(conf)
+        self.name = self.config['location']
+        self.location = self.config['location']
+        self.promise = self.config['promise']
+        self.network = self.config['network']
+        self.printInfo = True
+        self.printDebug = True
+        self.printError = True
+        self.dataFilePrefix = 'sps'
+        self.shellySTR = 'Shelly'
+        self.bluettiSTR = ['AC180','AC2']
+        self.devices = []
 
-	######### SETUP ###############
+    ######### SETUP ###############
 
-	# reads json config file and returns it as dict
-	def getConfig(self, fn:str) -> Any:
-		# Read data from a JSON file
-		try:
-			with open(fn, "r") as json_file:
-				return json.load(json_file)
-		except Exception as e:
-			self.log_error(f"Error during reading {fn} file: {e}")
-			if 'devices' in fn.lower():
-				return []
-			else:
-				return {}
+    # reads json config file and returns it as dict
+    def getConfig(self, fn:str) -> Any:
+        # Read data from a JSON file
+        try:
+            with open(fn, "r") as json_file:
+                return json.load(json_file)
+        except Exception as e:
+            self.log_error(f"Error during reading {fn} file: {e}")
+            if 'devices' in fn.lower():
+                return []
+            else:
+                return {}
 
-	######### BLUETOOTH ############
-	def reset_bluetooth(self) -> None:
-		try:
-			subprocess.run(["sudo", "hciconfig", "hci0", "up"], check=True)
-			subprocess.run(["sudo", "rfkill", "unblock", "bluetooth"], check=True)
-		except subprocess.CalledProcessError as e:
-			self.log_error(f"Bluetooth interface reset failed: {e}")
+    ######### BLUETOOTH ############
+    def reset_bluetooth(self) -> None:
+        try:
+            subprocess.run(["sudo", "hciconfig", "hci0", "up"], check=True)
+            subprocess.run(["sudo", "rfkill", "unblock", "bluetooth"], check=True)
+        except subprocess.CalledProcessError as e:
+            self.log_error(f"Bluetooth interface reset failed: {e}")
 
-	# get list of devices from device file, filtered by location
-	def getDevices(self, dF:str, location:str =self.Location)->list:
+    # get list of devices from device file, filtered by location
+    def getDevices(self, dF:str, location:str =self.Location)->list:
 
-		self.reset_bluetooth()
+        self.reset_bluetooth()
 
         self.log_debug(location)
 
@@ -66,180 +66,180 @@ class SmartPowerStation():
 
         return self.devices
 
-	# returns list of BLE objects and matching saved devices i.e. [BLE, saved]
-	async def scan_devices(self, saved_devices: Dict):
-	    filteredDevices = []
+    # returns list of BLE objects and matching saved devices i.e. [BLE, saved]
+    async def scan_devices(self, saved_devices: Dict):
+        filteredDevices = []
         scan_duration = 5
 
-	    addressList = []
-	    def discovery_handler(device: BLEDevice, advertisement_data: AdvertisementData):
-	        # mf = ''
-	        # notFound = 1
+        addressList = []
+        def discovery_handler(device: BLEDevice, advertisement_data: AdvertisementData):
+            # mf = ''
+            # notFound = 1
 
-	        if device.name is None:
-	            return
+            if device.name is None:
+                return
 
-	        for sd in saved_devices:
-	            #print(sd)
-	            if device.address == sd['address'] and device.address not in addressList:    
-	                self.log_debug(device)
-	                addressList.append(device.address)
-	                filteredDevices.append([device,sd])
+            for sd in saved_devices:
+                #print(sd)
+                if device.address == sd['address'] and device.address not in addressList:    
+                    self.log_debug(device)
+                    addressList.append(device.address)
+                    filteredDevices.append([device,sd])
 
-	    self.log_info(f"Scanning for BLE devices for {scan_duration} seconds...")
+        self.log_info(f"Scanning for BLE devices for {scan_duration} seconds...")
 
-	    async with BleakScanner(adapter=bleAdapter, detection_callback=discovery_handler) as scanner:
-	        await asyncio.sleep(scan_duration)
-	    
-	    self.log_debug(addressList)
+        async with BleakScanner(adapter=bleAdapter, detection_callback=discovery_handler) as scanner:
+            await asyncio.sleep(scan_duration)
+        
+        self.log_debug(addressList)
 
-	    # Some BLE chipsets (especially on Raspberry Pi) need a few seconds between scanning and connecting.
-	    await asyncio.sleep(2)
-	    
-	    return filteredDevices
-
-	# ============================
-	# Utilities
-	# ============================
-	def handle_signal(self, signal_num: int, frame: Any) -> None:
-	    """Handles termination signals for graceful shutdown."""
-	    self.log_info(f"Received signal {signal_num}, shutting down gracefully...")
-	    sys.exit(0)
-
-	def log_info(self, message: str) -> None:
-	    """Logs an info message."""
-	    logging.info(message)
-	    self.log_print(message, self.printInfo)
-
-	def log_error(self, message: str) -> None:
-	    """Logs an error message."""
-	    logging.error(message)
-	    self.log_print(message, self.printError)
-
-	def log_debug(self, message: str) -> None:
-	    """Logs a debug message."""
-	    logging.debug(message)
-	    self.log_print(message, self.printDebug)
-
-	def log_print(self, message:str, b:bool) -> None:
-	    if b:
-	        print(message)
-
-	# ============================
-	# Data
-	# ============================
-	#Check if the timestamp is within the last 10 minutes.
-	def isRecent(ts, seconds=600)->bool
-		if isinstance(ts, str):
-			ts = datetime.strptime(ts, "%Y-%m-%d %H:%M:%S") #check if the ts is a string and convert
-	    now = datetime.now()
-	    return now - ts <= timedelta(seconds)
-
-	def packageData(self, d, r, t) -> Dict:
-	    try:
-	        if d[1]['manufacturer'].lower() == 'bluetti':
-	            #print('bluetti!')
-	            t["powerstation_percentage"] = r['total_battery_percent']
-	            t["powerstation_inputWAC"] = r['ac_input_power']
-	            t["powerstation_inputWDC"] = r['dc_input_power']
-	            t["powerstation_outputWAC"] = r['ac_output_power']
-	            t["powerstation_outputWDC"] = r['dc_output_power']
-	            t["powerstation_outputMode"] = r['output_mode']
-	            t["powerstation_deviceType"] = r['device_type']
-	        elif 'Shelly'.lower() in d[1]['name'].lower():
-	            if '1PM'.lower() in d[1]['name'].lower():
-	                #print('1pm!')
-	                if d[1]['assignment0'] == 1:
-	                    t['relay1_power'] = r[0]["apower"]
-	                    t['relay1_current'] =r[0]["current"]
-	                    t['relay1_voltage'] =r[0]["voltage"]
-	                    t['relay1_status'] =str(r[0]["output"]) #must be cast to str because the dict interprets the bool as an int
-	                    t['relay1_device'] = d[1]['name']
-	                else:
-	                    t['relay2_power'] = r[0]["apower"]
-	                    t['relay2_current'] =r[0]["current"]
-	                    t['relay2_voltage'] =r[0]["voltage"]
-	                    t['relay2_status'] =str(r[0]["output"]) #must be cast to str because the dict interprets the bool as an int
-	                    t['relay2_device'] = d[1]['name']
-	            elif '2PM'.lower() in d[1]['name'].lower():
-	                #print('2pm!')
-	                t['relay1_power'] = r[0]["apower"]
-	                t['relay1_current'] =r[0]["current"]
-	                t['relay1_voltage'] =r[0]["voltage"]
-	                t['relay1_status'] =str(r[0]["output"]) #must be cast to str because the dict interprets the bool as an int
-	                t['relay1_device'] = d[1]['name']
-	                t['relay2_power'] = r[1]["apower"]
-	                t['relay2_current'] =r[1]["current"]
-	                t['relay2_voltage'] =r[1]["voltage"]
-	                t['relay2_status'] =str(r[1]["output"]) #must be cast to str because the dict interprets the bool as an int
-	                t['relay2_device'] = d[1]['name']
-	    except Exception as e:
-	        print(e)
-
-	    return t
+        # Some BLE chipsets (especially on Raspberry Pi) need a few seconds between scanning and connecting.
+        await asyncio.sleep(2)
+        
+        return filteredDevices
 
     # ============================
-	# Control
-	# ============================
+    # Utilities
+    # ============================
+    def handle_signal(self, signal_num: int, frame: Any) -> None:
+        """Handles termination signals for graceful shutdown."""
+        self.log_info(f"Received signal {signal_num}, shutting down gracefully...")
+        sys.exit(0)
 
-	def gridToBattery(self, state: bool) -> None:
-	    SPS.reset_bluetooth()
+    def log_info(self, message: str) -> None:
+        """Logs an info message."""
+        logging.info(message)
+        self.log_print(message, self.printInfo)
 
-	    location = SPS.location
-	    print(location)
+    def log_error(self, message: str) -> None:
+        """Logs an error message."""
+        logging.error(message)
+        self.log_print(message, self.printError)
 
-	    scan_duration = 5
-	    # Read data from a JSON file
-	    try:
-	        with open(deviceFile, "r") as json_file:
-	            savedDevices = json.load(json_file)
-	    except Exception as e:
-	        log_error(f"Error during reading devices.json file: {e}")
-	        savedDevices = []
+    def log_debug(self, message: str) -> None:
+        """Logs a debug message."""
+        logging.debug(message)
+        self.log_print(message, self.printDebug)
 
-	    filteredEntries = []
-	    for entry in savedDevices:
-	        if entry['location'] == location:
-	            filteredEntries.append(entry)
+    def log_print(self, message:str, b:bool) -> None:
+        if b:
+            print(message)
 
-	    try:
-	        devices = await scan_devices(scan_duration, filteredEntries)
-	    except Exception as e:
-	        log_error(f"Error during scanning: {e}")
-	        return
+    # ============================
+    # Data
+    # ============================
+    #Check if the timestamp is within the last 10 minutes.
+    def isRecent(ts, seconds=600)->bool
+        if isinstance(ts, str):
+            ts = datetime.strptime(ts, "%Y-%m-%d %H:%M:%S") #check if the ts is a string and convert
+        now = datetime.now()
+        return now - ts <= timedelta(seconds)
 
-	    if not devices:
-	        log_error("No devices found. Exiting")
-	        sys.exit(0)
+    def packageData(self, d, r, t) -> Dict:
+        try:
+            if d[1]['manufacturer'].lower() == 'bluetti':
+                #print('bluetti!')
+                t["powerstation_percentage"] = r['total_battery_percent']
+                t["powerstation_inputWAC"] = r['ac_input_power']
+                t["powerstation_inputWDC"] = r['dc_input_power']
+                t["powerstation_outputWAC"] = r['ac_output_power']
+                t["powerstation_outputWDC"] = r['dc_output_power']
+                t["powerstation_outputMode"] = r['output_mode']
+                t["powerstation_deviceType"] = r['device_type']
+            elif 'Shelly'.lower() in d[1]['name'].lower():
+                if '1PM'.lower() in d[1]['name'].lower():
+                    #print('1pm!')
+                    if d[1]['assignment0'] == 1:
+                        t['relay1_power'] = r[0]["apower"]
+                        t['relay1_current'] =r[0]["current"]
+                        t['relay1_voltage'] =r[0]["voltage"]
+                        t['relay1_status'] =str(r[0]["output"]) #must be cast to str because the dict interprets the bool as an int
+                        t['relay1_device'] = d[1]['name']
+                    else:
+                        t['relay2_power'] = r[0]["apower"]
+                        t['relay2_current'] =r[0]["current"]
+                        t['relay2_voltage'] =r[0]["voltage"]
+                        t['relay2_status'] =str(r[0]["output"]) #must be cast to str because the dict interprets the bool as an int
+                        t['relay2_device'] = d[1]['name']
+                elif '2PM'.lower() in d[1]['name'].lower():
+                    #print('2pm!')
+                    t['relay1_power'] = r[0]["apower"]
+                    t['relay1_current'] =r[0]["current"]
+                    t['relay1_voltage'] =r[0]["voltage"]
+                    t['relay1_status'] =str(r[0]["output"]) #must be cast to str because the dict interprets the bool as an int
+                    t['relay1_device'] = d[1]['name']
+                    t['relay2_power'] = r[1]["apower"]
+                    t['relay2_current'] =r[1]["current"]
+                    t['relay2_voltage'] =r[1]["voltage"]
+                    t['relay2_status'] =str(r[1]["output"]) #must be cast to str because the dict interprets the bool as an int
+                    t['relay2_device'] = d[1]['name']
+        except Exception as e:
+            print(e)
 
-	    for d in devices:
-	        print(d)
-	        # shDevice = await statusUpdate(d)
-	        # if shDevice:
-	        #     print(shDevice.status)
-	        #     c = list(range(shDevice.channels))
-	        #     print(await shDevice.execute_command(10,c))
-	        bleDev = d[0]
-	        savedDev = d[1]
+        return t
 
-	        if savedDev['manufacturer'] == 'shelly':
+    # ============================
+    # Control
+    # ============================
 
-	            shDevice = ShellyDevice(savedDev["address"], savedDev["name"])
-	            try:
-	                await shDevice.setState(toState,0)
-	            except Exception as e:
-	                log_error(f"Error setting state")
+    def gridToBattery(self, state: bool) -> None:
+        SPS.reset_bluetooth()
+
+        location = SPS.location
+        print(location)
+
+        scan_duration = 5
+        # Read data from a JSON file
+        try:
+            with open(deviceFile, "r") as json_file:
+                savedDevices = json.load(json_file)
+        except Exception as e:
+            log_error(f"Error during reading devices.json file: {e}")
+            savedDevices = []
+
+        filteredEntries = []
+        for entry in savedDevices:
+            if entry['location'] == location:
+                filteredEntries.append(entry)
+
+        try:
+            devices = await scan_devices(scan_duration, filteredEntries)
+        except Exception as e:
+            log_error(f"Error during scanning: {e}")
+            return
+
+        if not devices:
+            log_error("No devices found. Exiting")
+            sys.exit(0)
+
+        for d in devices:
+            print(d)
+            # shDevice = await statusUpdate(d)
+            # if shDevice:
+            #     print(shDevice.status)
+            #     c = list(range(shDevice.channels))
+            #     print(await shDevice.execute_command(10,c))
+            bleDev = d[0]
+            savedDev = d[1]
+
+            if savedDev['manufacturer'] == 'shelly':
+
+                shDevice = ShellyDevice(savedDev["address"], savedDev["name"])
+                try:
+                    await shDevice.setState(toState,0)
+                except Exception as e:
+                    log_error(f"Error setting state")
 
 
-	# turn grid to load connection on or off
-	def gridToLoad(self, state: bool) -> None:
-	    # go through device list
+    # turn grid to load connection on or off
+    def gridToLoad(self, state: bool) -> None:
+        # go through device list
 
-	    #grab device assigned to position 2
+        #grab device assigned to position 2
 
 
-	    #set state based on bool
-	    if state:
-	        #turn on
-	    else:
-	        #turn off 
+        #set state based on bool
+        if state:
+            #turn on
+        else:
+            #turn off 

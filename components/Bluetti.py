3d0ab1eca305e32d35959659cd97f7bac2e8cc93
alexnathanson <alex@alexnathanson.com>
2025-04-28 15:43:38 -0400

    cleaned up bluetti class

--- components/Bluetti.py
+++ components/Bluetti.py
@@ -1,12 +1,12 @@
-import argparse
+#import argparse
 import asyncio
-import base64
+#import base64
 from bleak import BleakError
 #from io import TextIOWrapper
-import json
+#import json
 import sys
-import textwrap
-import time
+#import textwrap
+#import time
 from typing import cast
 from bluetti_mqtt.bluetooth import (
     check_addresses, build_device, scan_devices, BluetoothClient, ModbusError,
@@ -22,68 +22,67 @@ class Bluetti():
         self.name = name
         self.manufacturer = 'bluetti'
         self.data = {'total_battery_percent':0,'ac_output_power':0,'ac_input_power':0,'dc_output_power':0,'dc_input_power':0}
-        self.charge = False
+        #self.charge = False
         self.BLUETTI_GATT_SERVICE_UUID = "0000ff00-0000-1000-8000-00805f9b34fb" #not in use
-    
-    # def manageCharge(self, charge: self.charge, percent: self.data['total_battery_percent']):
-    # # update charge state flag
-    #     # if battery is <= 20 start charging until full
-    #     if percent <= 20:
-    #         chargeState = 1
-    #     elif battery.percentage == 100:
-    #         chargeState = 0
 
     async def log_command(self, client: BluetoothClient, device: BluettiDevice, command: DeviceCommand):
-        response_future = await client.perform(command)
         try:
+            response_future = await client.perform(command)
             response = cast(bytes, await response_future)
             if isinstance(command, ReadHoldingRegisters):
                 body = command.parse_response(response)
                 parsed = device.parse(command.starting_address, body)
                 return parsed #print(parsed.keys())
-            #log_packet(log_file, response, command)
         except (BadConnectionError, BleakError, ModbusError, ParseError) as err:
             print(f'Got an error running command {command}: {err}')
-            #log_invalid(log_file, err, command)
 
     async def run(self):
 
         devices = await check_addresses({self.mac})
-        if len(devices) == 0:
+        if not devices:
             sys.exit('Could not find the given device to connect to')
         device = devices[0]
 
         print(f'Connecting to {device.address}')
         client = BluetoothClient(device.address)
         #asyncio.get_running_loop().create_task(client.run())
-        client.run()
+        async with asyncio.TaskGroup() as tg:
+            tg.create_task(client.run())
         
-        # Wait for device connection
-        while not client.is_ready:
-            print('Waiting for connection...')
-            await asyncio.sleep(1)
-            continue
-        print('Bluetti device is ready')
+            # Wait for device connection
+            # while not client.is_ready:
+            #     print('Waiting for connection...')
+            #     await asyncio.sleep(1)
+            #     continue
 
-        # Poll device
-        #while True:
-        for command in device.logging_commands:
-            commandResponse = await self.log_command(client, device, command)
-            for k,v in commandResponse.items():
-                #print(k + ": " + str(v))
-                self.data[k]=v
-            #await asyncio.sleep(freq)
+
+            max_tries = 10
+            for _ in range(max_tries):
+                if client.is_ready:
+                    break
+                print('Waiting for connection...')
+                await asyncio.sleep(1)
+            else:
+                print('Connection timeout')
+                return
+
+            print('Bluetti device is ready')
+
+            # Poll device
+            #while True:
+            for command in device.logging_commands:
+                commandResponse = await self.log_command(client, device, command)
+                if commandResponse:
+                    for k,v in commandResponse.items():
+                        #print(k + ": " + str(v))
+                        self.data[k]=v
+                #await asyncio.sleep(freq)
 
     async def getStatus(self):
-        address = self.address
         myData={
         }
 
         try:
-            # devices = await check_addresses({address})
-            # #if len(devices) == 0:
-            #   #  sys.exit('Could not find the given device to connect to')
-            # device = devices[0]
             device = build_device(self.address, self.name)
 
             print(f'Connecting to {self.address}')
@@ -102,34 +101,44 @@ class Bluetti():
             for sig in (signal.SIGINT, signal.SIGTERM):
                 loop.add_signal_handler(sig, shutdown)
 
-            async with asyncio.TaskGroup() as tg:
-                # Start the client
-                tg.create_task(client.run())
+            try:
+                async with asyncio.TaskGroup() as tg:
+                    # Start the client
+                    tg.create_task(client.run())
 
-                # Wait for a shutdown signal
-                await stop_event.wait()
+                    # Wait for a shutdown signal
+                    await stop_event.wait()
 
-            # Wait for device connection
-            maxTries = 10
-            t = 0
-            while not client.is_ready:
-                print('Waiting for connection...')
-                await asyncio.sleep(1)
-                t = t +1
-                if t > 10:
-                    break
-                continue
+                # Wait for device connection
+                maxTries = 10
+                # t = 0
+                # while not client.is_ready:
+                #     print('Waiting for connection...')
+                #     await asyncio.sleep(1)
+                #     t = t +1
+                #     if t > 10:
+                #         break
+                #     continue
+                for t in range(maxTries):
+                    if client.is_ready:
+                        break
+                    print('Waiting for connection...')
+                    await asyncio.sleep(1)
+                else:
+                    print('Connection timeout')
+                    return myData
 
-            # Poll device
-            for command in device.logging_commands:
-                commandResponse = await self.log_command(client, device, command)
-                for k,v in commandResponse.items():
-                    myData[k]=v
-            #print(myData)
-            await client.client.disconnect()
-
-            return myData
-
-
+                # Poll device
+                for command in device.logging_commands:
+                    commandResponse = await self.log_command(client, device, command)
+                    if commandResponse:
+                        for k,v in commandResponse.items():
+                            myData[k]=v
+                #print(myData)
         except Exception as e:
             print(f"Unexpected error during command execution: {e}")
+        final:
+            if client.client.is_connected:
+                await client.client.disconnect()
+
+        return myData

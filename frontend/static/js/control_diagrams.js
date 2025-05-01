e8e0a747450d357a8e3608a4f39a72e04c11b07f
alexnathanson <alex@alexnathanson.com>
2025-05-01 12:20:21 -0400

    image

--- frontend/static/js/control_diagrams.js
+++ frontend/static/js/control_diagrams.js
@@ -1,24 +1,24 @@
 let img;
-    let currentImage = 'static/images/image1.jpg';  // Initial image path
+let currentImage = 'static/images/image1.jpg';  // Initial image path
 
-    function setup() {
-        let canvas = createCanvas(400, 300);
-        canvas.parent('controlLogicContainer');  // Attach canvas to the div
-        loadImage(currentImage, (loadedImg) => {
-            img = loadedImg;
-        });
-    }
+function setup() {
+    let canvas = createCanvas(400, 300);
+    canvas.parent('controlLogicContainer');  // Attach canvas to the div
+    loadImage(currentImage, (loadedImg) => {
+        img = loadedImg;
+    });
+}
 
-    function draw() {
-        background(255);
-        if (img) {
-            image(img, 0, 0, width, height);
-        }
+function draw() {
+    background(255);
+    if (img) {
+        image(img, 0, 0, width, height);
     }
+}
 
-    function changeImage(filename) {
-        const fullPath = `static/images/${filename}`;
-        loadImage(fullPath, (loadedImg) => {
-            img = loadedImg;
-        });
-    }
+function changeImage(filename) {
+    const fullPath = `static/images/${filename}`;
+    loadImage(fullPath, (loadedImg) => {
+        img = loadedImg;
+    });
+}
